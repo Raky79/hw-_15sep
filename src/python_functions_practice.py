6c4de46b8674f84390adb32c3c14eb88b4a8ef23
Raquel Revelles <raquel@Raquels-MacBook-Pro.local>
2022-09-15 09:09:10 +0100

    my initial commit

---
+++ src/python_functions_practice.py
@@ -0,0 +1,44 @@
+def return_10():
+    return 10
+
+def add(num_1, num_2):
+    result = num_1 + num_2
+    return result
+
+
+def subtract(num_10, num_5):
+    result = num_10 - num_5
+    return result
+    
+def multiply(num_4, num_2):
+    result = num_4 * num_2
+    return result
+     
+def divide(num_10, num_2):
+      result = num_10 // num_2
+      return result
+
+  
+def length_of_string(test_string ):
+    result = len("A string of length 21")
+    return result
+
+def join_string(string_1, string_2):
+    result = string_1 + string_2
+    return result 
+
+
+def add_string_as_number(num_1, num_2):
+    num_1 = int("1")
+    num_2 = int("2")
+    result = num_1 + num_2
+    return result
+
+
+
+
+
+
+
+
+
